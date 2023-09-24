c0029e456b35bbed65e768ebb0ad1e7554fff2a8
rahuljchopra <choprarahul00@gmail.com>
2023-09-24 18:05:52 -0400

    Updated application with two Flask-Moment timestamps

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
+from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -14,7 +17,8 @@ def internal_server_error(e):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
